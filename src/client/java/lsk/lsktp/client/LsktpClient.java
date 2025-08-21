d4dc11de4df4e352f790e3b88233d6004c339ce7
承杰 吴 <jerry37032@outlook.com>
2025-08-21 18:23:21 +0800

    0.4.1-1.21.8fabric

---
+++ src/client/java/lsk/lsktp/client/LsktpClient.java
@@ -0,0 +1,10 @@
+package lsk.lsktp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LsktpClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/lsk/lsktp/Lsktp.java
@@ -0,0 +1,47 @@
+package lsk.lsktp;
+
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.util.HashSet;
+
+public class Lsktp implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("lsktp")
+                    .then(CommandManager.argument("x", DoubleArgumentType.doubleArg())
+                            .then(CommandManager.argument("y", DoubleArgumentType.doubleArg())
+                                    .then(CommandManager.argument("z", DoubleArgumentType.doubleArg())
+                                            //   .executes(context -> teleportPlayer(context))
+                                            .executes(context ->execute_command(context.getSource(),context))
+                                    )
+                            )
+                    )
+                    .requires(source -> source.hasPermissionLevel(0)) // 设置权限等级
+            );
+        });
+
+    }
+    static int execute_command(ServerCommandSource Source, CommandContext<ServerCommandSource> context){
+        Entity entity =  Source.getPlayer() ;
+
+        double x = DoubleArgumentType.getDouble(context, "x");
+        double y = DoubleArgumentType.getDouble(context, "y");
+        double z = DoubleArgumentType.getDouble(context, "z");
+
+        entity.teleport(Source.getWorld(),x , y, z, new HashSet<>(), entity.getYaw(), entity.getPitch(),false);
+//      return execute_command(Source, Radius, Player, Origin.getPos());
+        context.getSource().sendFeedback(() -> Text.literal("传送至 " + x + ", " + y + ", " + z), false);
+
+        return 1;
+    }
+}
+
