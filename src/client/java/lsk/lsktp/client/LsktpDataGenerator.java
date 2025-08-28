6e64deae5296eb4bf8449a6b1fe0a719be2183af
承杰 吴 <jerry37032@outlook.com>
2025-08-28 11:31:11 +0800

    0.4.1

---
+++ src/client/java/lsk/lsktp/client/LsktpDataGenerator.java
@@ -0,0 +1,12 @@
+package lsk.lsktp.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LsktpDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
