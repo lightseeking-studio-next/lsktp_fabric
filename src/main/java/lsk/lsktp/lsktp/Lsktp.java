fb7952223bf54b68e40677e6ae26bb8ab5f68432
承杰 吴 <jerry37032@outlook.com>
2026-06-23 15:48:51 +0800

    1.0

--- src/main/java/lsk/lsktp/lsktp/Lsktp.java
+++ src/main/java/lsk/lsktp/lsktp/Lsktp.java
@@ -17,7 +17,6 @@ import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import java.util.Collections;
 
 public class Lsktp implements ModInitializer {
